d7a1133c36fa55a857e0d6faead72ea8d60b886d
lcech <ja@lukascech.cz>
2021-10-19 13:44:30 +0200

    Cookie deletion

--- unhide.js
+++ unhide.js
@@ -1,5 +1,15 @@
-(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-    })(window,document.documentElement,'async-hide','dataLayer',4000,
-    {'OPT-MMPWKF3':true});
+(function (a, s, y, n, c, h, i, d, e) {
+  s.className += " " + y;
+  h.start = 1 * new Date();
+  h.end = i = function () {
+    document.documentElement.className = document.documentElement.className.replace(RegExp(" ?async-hide"), "");
+  };
+  (a[n] = a[n] || []).hide = h;
+  setTimeout(function () {
+    i();
+    h.end = null;
+  }, c);
+  h.timeout = c;
+})(window, document.documentElement, "async-hide", "dataLayer", 4000, {
+  "OPT-MMPWKF3": true,
+});
